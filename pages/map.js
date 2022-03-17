42bcdd183b85e8bcde6c850c37bf5d5a57a18174
IM3 <leon.baldelli@cnrs.fr>
2022-03-17 20:07:33 +0100

    exposing cities

--- pages/map.js
+++ pages/map.js
@@ -5,13 +5,33 @@ export default function Map() {
     const d3map = useRef();
 
     useEffect(()=>{
-        fetch('data/seeds.json')
-        .then(response => response.json())
-        .then(data=>{
-            console.log(data)
-        })
-    
+        // const res = await Promise.all([
+        //     fetch('data/seeds.json').then(response => response.json()),
+        //     fetch('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson').then(response => response.json()),
+        // ].map(promise => promise.catch(error => error)))
+        Promise.all([
+            fetch('data/seeds.json').then(response => response.json()),
+            fetch('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson')
+                .then(response => response.json())
+        ]).then(data =>  {
+            // data.forEach(element => {
+            //     console.log(element)
+                console.log(data);
+                const worlddata = data[1];
+                const citydata = data[0];
+
+                console.log('worlddata', worlddata);
+                console.log('citydata', citydata);
+
+                let p = document.createElement('p');
+                p.textContent = citydata.map((v, k) => v.name);
+                document.getElementById('output').appendChild(p)
+            // });
+        }).catch(err => {
+
+        });
         const margin = {top: 50, right: 30, bottom: 30, left: 30}
+        // console.log(res)
 
         if (typeof window !== "undefined") {
             const width = parseInt(d3.select('#map').style('width'))
@@ -27,15 +47,19 @@ export default function Map() {
                             .append('g')
                                 .attr('transform', 'translate(' + margin.left + ','
                                     + margin.top + ')');
-
+            var projection = d3.geoMercator()
+                .center([0,20])                // GPS of location to zoom on
+                .scale(99)                       // This is like the zoom
+                .translate([ width/2, height/2 ]);
             console.log('background-color', d3.select('#map').style('background-color'))
             console.log(d3map.current)
         }
     })
 
 return (
-    <div id="map" style={{width: 300, height: 200}} ref={d3map}>
-      
-    </div>
+    <>
+        <div id="output"></div>
+        <div id="map" style={{ width: 300, height: 200 }} ref={d3map}></div>
+    </>
   )
 }
