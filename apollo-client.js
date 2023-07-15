b19079cd1a2d0bdd6d2ed42e29b73745e8c7c9b8
IM3 <leon.baldelli@cnrs.fr>
2023-07-15 15:32:27 +0200

    removing logic based on graph

--- apollo-client.js
+++ apollo-client.js
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
     uri: "https://api.thegraph.com/subgraphs/name/p40l051/the-system",
+    // uri: "",
     cache: new InMemoryCache(),
 });
 
